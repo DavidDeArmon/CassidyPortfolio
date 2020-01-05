8ec9b20b4c5a6a967b94bd03abd3552e3ca34e52
David DeArmon <david.dearmon@gmail.com>
2020-01-05 14:08:27 -0700

    structure changes

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,31 +1,49 @@
 import React from 'react'
 import EmailForm from './Email'
 import avatar from '../assets/images/avatar2.jpg'
+import Modals from '../components/Modals'
 
 
 
-function Main(props) {
-    return (
-        <div id="main">
-            <section id="one">
-                <header className="major">
-                    <h2>Cass Takes Photos</h2>
-                </header>
-                <div className ='avatar-container'>
-                 <a href="/" className="image avatar"><img src={avatar} alt="avatar"/></a>
-                 <h2 className="tagline"><strong>Hello</strong>, I'm Cassidy </h2>
-                </div>
-                <ul className="actions">
-                    <li><button onClick={() => { props.openModal('about') }} className="button medium">Get To Know Me</button></li>
-                    <li><button onClick={() => { props.openModal('recentWork') }} className="button medium">Gallery</button></li>
-                    <li><button onClick={() => { props.openModal('pricing') }} className="button medium">Pricing</button></li>
-                </ul>
-            </section>
-            <section id="two">
-            <h2>Get In Touch</h2>
-                <EmailForm/>
-            </section>
-        </div>
-    )
+class Main extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            modalIsOpen: false,
+            lightboxIsOpen: false,
+            modalToOpen: ''
+        };
+    }
+    openModal = (modalToOpen) => {
+        this.setState({ modalIsOpen: true, modalToOpen });
+    }
+    closeModal = () => {
+        this.setState({ modalIsOpen: false });
+    }
+    render() {
+        return (
+            <div id="main">
+                <section id="one">
+                <Modals modalIsOpen={this.state.modalIsOpen} closeModal={this.closeModal} modal={this.state.modalToOpen} />
+                    <header className="major">
+                        <h2>Cass Takes Photos</h2>
+                    </header>
+                    <div className='avatar-container'>
+                        <a href="/" className="image avatar"><img src={avatar} alt="avatar" /></a>
+                        <h2 className="tagline"><strong>Hello</strong>, I'm Cassidy </h2>
+                    </div>
+                    <ul className="actions">
+                        <li><button onClick={() => { this.openModal('about') }} className="button medium">Get To Know Me</button></li>
+                        <li><button onClick={() => { this.openModal('recentWork') }} className="button medium">Gallery</button></li>
+                        <li><button onClick={() => { this.openModal('pricing') }} className="button medium">Pricing</button></li>
+                    </ul>
+                </section>
+                <section id="two">
+                    <h2>Get In Touch</h2>
+                    <EmailForm />
+                </section>
+            </div>
+        )
+    }
 }
 export default Main
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,39 +1,20 @@
 import React from 'react'
 import Helmet from 'react-helmet'
-
 import Main from '../components/Main'
-import Layout from '../components/layout'
-import Modals from '../components/Modals'
+import Layout from '../components/Layout'
 
 
 class HomeIndex extends React.Component {
-    constructor() {
-        super();
-
-        this.state = {
-            modalIsOpen: false,
-            lightboxIsOpen: false,
-            modalToOpen:''
-        };
-    }
-    openModal = (modalToOpen) => {
-        this.setState({ modalIsOpen: true, modalToOpen });
-    }
-    closeModal = () => {
-        this.setState({ modalIsOpen: false });
-    }
     render() {
         const siteTitle = "Cass Takes Photos"
         const siteDescription = "Cassidy Smith's Portolio"
-
         return (
             <Layout>
                 <Helmet>
                     <title>{siteTitle}</title>
                     <meta name="description" content={siteDescription} />
                 </Helmet>
-                <Main openModal={this.openModal} toggleLightbox={this.toggleLightbox}  />
-                <Modals modalIsOpen={this.state.modalIsOpen} closeModal={this.closeModal} modal={this.state.modalToOpen}/>
+                <Main />
             </Layout>
         )
     }
