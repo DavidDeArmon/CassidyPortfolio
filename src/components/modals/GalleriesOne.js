eb4c2c9e4c8281694f0a3ad8e2da0c7044672c57
David DeArmon <david.dearmon@gmail.com>
2021-05-04 11:01:50 -0600

    updated gallery files

--- src/components/modals/GalleriesOne.js
+++ src/components/modals/GalleriesOne.js
@@ -1,21 +1,43 @@
 import React from 'react'
 import GalleriesTwo from './GalleriesTwo';
 
-import engage1 from '../../assets/images/engagements/emmaChris (11).webp'
-import bridal1 from '../../assets/images/bridals/dannyMelissa (1).webp'
-import wedding1 from '../../assets/images/weddings/kamKatie (7).webp'
-import portrait1 from '../../assets/images/portraits/alyssa (1).webp'
-import family1 from '../../assets/images/families/gallardoFamily (9).webp'
-import maternity1 from '../../assets/images/maternity/abbyBen (2).webp'
+import emmaChris from '../../assets/images/engagements/emmaChris (11).webp'
+import jaredAnnika from '../../assets/images/engagements/jaredAnnika (11).webp'
+import jeffAshley from '../../assets/images/engagements/jeffAshley (20).webp'
+import dannyMelissa from '../../assets/images/bridals/dannyMelissa (3).webp'
+import treyKyndell from '../../assets/images/bridals/treyKyndell (7).webp'
+import kamKatie from '../../assets/images/weddings/kamKatie (7).webp'
+import kysonAshlee from '../../assets/images/weddings/kysonAshlee (32).webp'
+import nateMegan from '../../assets/images/weddings/nateMegan (1).webp'
+import alyssa from '../../assets/images/portraits/alyssa (1).webp'
+import amy from '../../assets/images/portraits/amy (9).webp'
+import gallardoFamily from '../../assets/images/families/gallardoFamily (9).webp'
+import larsenFamily from '../../assets/images/families/larsenFamily (5).webp'
+import abbyBen from '../../assets/images/maternity/abbyBen (2).webp'
+import dallinMarisa from '../../assets/images/maternity/dallinMarisa (1).webp'
+import dannyPeyton from '../../assets/images/maternity/dannyPeyton (3).webp'
+import jonHaylie from '../../assets/images/jonHaylie/jonHaylie (7).webp'
+import ellora from '../../assets/images/ellora/ellora (1).webp'
 
 
 const CATEGORIES = [
-    { id: 1, thumbnail: engage1, title: 'Engagements' },
-    { id: 2, thumbnail: bridal1, title: 'Bridals' },
-    { id: 3, thumbnail: wedding1, title: 'Weddings' },
-    { id: 4, thumbnail: portrait1, title: 'Portraits' },
-    { id: 5, thumbnail: family1, title: 'Families' },
-    { id: 6, thumbnail: maternity1, title: 'Maternity' },
+    { id: 1, thumbnail: emmaChris, title: 'Emma & Chris' },
+    { id: 2, thumbnail: jaredAnnika, title: 'Jared & Annika' },
+    { id: 3, thumbnail: jeffAshley, title: 'Jeff & Ashley' },
+    { id: 4, thumbnail: dannyMelissa, title: 'Danny & Melissa' },
+    { id: 5, thumbnail: treyKyndell, title: 'Trey & Kyndell' },
+    { id: 6, thumbnail: kamKatie, title: 'Kam & Katie' },
+    { id: 7, thumbnail: kysonAshlee, title: 'Kyson & Ashlee' },
+    { id: 8, thumbnail: nateMegan, title: 'Nate & Megan' },
+    { id: 9, thumbnail: alyssa, title: 'Alyssa' },
+    { id: 10, thumbnail: amy, title: 'Amy' },
+    { id: 11, thumbnail: gallardoFamily, title: 'Gallardo Family' },
+    { id: 12, thumbnail: larsenFamily, title: 'Larsen Family' },
+    { id: 13, thumbnail: abbyBen, title: 'Abby & Ben' },
+    { id: 14, thumbnail: dallinMarisa, title: 'Dallin & Marisa' },
+    { id: 15, thumbnail: dannyPeyton, title: 'Danny & Peyton' },
+    { id: 16, thumbnail: jonHaylie, title: 'Jon & Hailey' },
+    { id: 17, thumbnail: ellora, title: 'Ellora' },
 ];
 
 class GalleriesOne extends React.Component {
@@ -36,19 +58,19 @@ class GalleriesOne extends React.Component {
     createGallery(images) {
         if (images.length<1) return;
 
-        const gallery = images.map((pic) => {
+        const gallery = images.map((pic,key) => {
             return (
                 <a
-                    key={pic.id}
+                    key={key}
                     className="image thumb"
                     id="noFlex"
                     href={pic.source}
                     onClick={e => {
                         e.preventDefault();
-                        this.props.toggleViewer(pic.id);
+                        this.props.toggleViewer(key);
                     }}
                 >
-                    <img  src={pic.thumbnail} alt={pic.caption} />
+                    <img  src={pic.source} alt={pic.caption} />
                 </a>
             );
         });
