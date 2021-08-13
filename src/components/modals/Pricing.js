dbf35a5d50c09741f4b1d2b0d21d37a4083ac721
David DeArmon <david.dearmon@gmail.com>
2021-08-12 23:29:42 -0600

    changed pricing info according to email

--- src/components/modals/Pricing.js
+++ src/components/modals/Pricing.js
@@ -12,14 +12,14 @@ function Pricing() {
   
     const pricingModel = [
         // { id: 0, thumbnail: '', title: "", price: "Price", photos: "# Photos", details: "Details" },
-        { id: 1, thumbnail: creative, title: "Portraits", price: "$100", details: "1 Hour" },
-        { id: 2, thumbnail: familesOne, title: "Familes", price: "$175 ", details: "$15/Person After 3" },
-        { id: 3, thumbnail: engagements, title: "Engagements/Couples", price: "$150", details: "1 Hour" },
-        { id: 4, thumbnail: firstLook, title: "Bridals", price: "$200", details: "1 Hour" },
-        { id: 5, thumbnail: sealing, title: "Sealing Only", price: "$350", details: "Exit, Family Photos, Bridals at Temple" },
-        { id: 6, thumbnail: ceremony, title: "Ceremony", price: "$500", details: "Ceremony, Family Photos, Bridals at Location" },
-        { id: 7, thumbnail: fullWeddingCoverage, title: "Wedding Day Coverage", price: "$950", details: "$100/Hr After 4 Hours" },
-        { id: 8, thumbnail: fullWeddingPackage, title: "Full Wedding Package", price: "$1200", details: "Engagements, Bridals, Wedding Day Coverage" },
+        { id: 1, thumbnail: creative, title: "Portraits", price: "$100", details: "1 Hr" },
+        { id: 2, thumbnail: familesOne, title: "Families", price: "$250 ", details: "45 minutes (more time added for more family members) +20 per person " },
+        { id: 3, thumbnail: engagements, title: "Engagements/Couples", price: "$250", details: "1 Hr, 2 outfits" },
+        { id: 4, thumbnail: firstLook, title: "Bridals", price: "$300", details: "1.5 Hrs" },
+        { id: 5, thumbnail: sealing, title: "I love you Wedding Package", price: "$600", details: "2 hrs, exit, family photos & bridals at temple" },
+        { id: 6, thumbnail: ceremony, title: "I love you lots Wedding Package", price: "$800", details: "2.5 hrs, ceremony, family photos & bridals at the venue" },
+        { id: 7, thumbnail: fullWeddingCoverage, title: "I love you more Wedding Package", price: "$1500", details: "5 hrs, sealing/ceremony & reception coverage" },
+        { id: 8, thumbnail: fullWeddingPackage, title: "I love you most Wedding Package", price: "$2000", details: "sealing/ceremony, reception, bridals & engagements " },
     ]
 
     let displayModel = pricingModel.map((e) => <PricingCard key={e.id} thumbnail={e.thumbnail} title={e.title} price={e.price} photos={e.photos} details={e.details} />);
@@ -28,7 +28,7 @@ function Pricing() {
         <div className="pricing" id="box">
             <h2>Pricing</h2>
             {displayModel}
-            <h5>*Additional Travel fees apply outside of Utah County. Contact me for details.</h5>
+            <h5>*Taxes are factored into the price for each session and package. </h5>
         </div>
     )
 }
